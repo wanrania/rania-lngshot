21cefb0be53b0466988c7eb4c581316f66753f98
wanrania <wan24si@mahasiswa.pcr.ac.id>
2026-04-04 18:03:40 +0700

    pertemuan 3

---
+++ app/src/main/java/com/example/rania_lngshot/pertemuan_2/LoginActivity.kt
@@ -0,0 +1,32 @@
+package com.example.rania_lngshot.pertemuan_2
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rania_lngshot.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val username = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                val intent = Intent(this, WelcomeActivity::class.java)
+                intent.putExtra("USERNAME", username)
+                startActivity(intent)
+            } else {
+                binding.etUsername.error = "Wajib diisi"
+                binding.etPassword.error = "Wajib diisi"
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/rania_lngshot/pertemuan_2/WelcomeActivity.kt
@@ -0,0 +1,21 @@
+package com.example.rania_lngshot.pertemuan_2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rania_lngshot.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val username = intent.getStringExtra("USERNAME")
+
+        binding.tvWelcome.text = "Selamat Datang, $username 🎉"
+    }
+}
